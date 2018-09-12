dc2e46e9e44832c364eb86e95a5d5c0bcfda69be
Milene Pereira Guimarães de Jezuz <milene.guimaraes@b2wdigital.com>
2018-09-12 11:45:55 -0300

    New feature: get Protein Data List

---
+++ src/main/java/com/bioinfo/dto/ProteinData.java
@@ -0,0 +1,23 @@
+package com.bioinfo.dto;
+
+public class ProteinData {
+
+    private String geneId;
+    private String aminoacidData;
+
+    public String getGeneId() {
+        return geneId;
+    }
+
+    public void setGeneId(String geneId) {
+        this.geneId = geneId;
+    }
+
+    public String getAminoacidData() {
+        return aminoacidData;
+    }
+
+    public void setAminoacidData(String aminoacidData) {
+        this.aminoacidData = aminoacidData;
+    }
+}
